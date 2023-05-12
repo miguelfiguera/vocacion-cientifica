46726586e45e7b6e53e02cb7721032a5f52b81b7
Miguel Figuera <miguelqui725@gmail.com>
2023-05-11 21:04:38 -0400

    dark the headers opacity

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,13 +16,16 @@ window.addEventListener('resize',()=>{
 window.addEventListener('locationchange', ()=> {
 	setCurrentUrl(window.location.pathname)
 });
+
+//add current URL logic to change header title
+
 return(
 
     <div className=" my-0 py-0 justify-content-center align-content-center" style={{backgroundImage:`url(${headerBg})`,backgroundSize:'cover',height:bannerHeight}}>
 
-<div className="container-fluid" style={{backgroundColor:'black'}}>
-    <strong><h1 className=" my-0 py-0 text-light" style={{opacity:'1'}}> Hola!</h1></strong>
+<div className="container-fluid " style={{backgroundColor:'black',height:bannerHeight,opacity:'0.5'}}>
     </div>
+    <strong><h1 className=" my-0 py-0 text-light fs-1" style={{scale:'1.2',backgroundColor:'transparent',translate:'0% -600%',scale:'2'}}> Hola!</h1></strong>
 
 
 
